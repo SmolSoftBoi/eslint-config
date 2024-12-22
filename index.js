40614e72fd473493345a7e7bc687dc216b42f9b1
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2024-12-22 18:26:33 +0000

    Refactor ESLint configuration to use ES module syntax and update peer dependencies

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "eslint-config-typescript-sort-imports",
-  ],
-};
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+];
