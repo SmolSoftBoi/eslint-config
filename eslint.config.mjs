bd376fe4025ebe212f3436943c7795d7c915f185
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2026-05-01 21:38:55 +0100

    Use import-x resolver-next in ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 
 import config from "./index.mjs";
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
 import typescriptEslint from 'typescript-eslint';
+import { createNodeResolver } from 'eslint-plugin-import-x';
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default typescriptEslint.config(
@@ -23,14 +25,14 @@ export default typescriptEslint.config(
   {
     // Ensure eslint-plugin-import-x can resolve ESM + TS ecosystem deps in this repo.
     settings: {
-      'import-x/resolver': {
-        node: {
-          extensions: ['.js', '.mjs', '.cjs', '.ts', '.tsx']
-        },
-        typescript: {
+      'import-x/resolver-next': [
+        createTypeScriptImportResolver({
           alwaysTryTypes: true
-        }
-      }
+        }),
+        createNodeResolver({
+          extensions: ['.js', '.mjs', '.cjs', '.ts', '.tsx']
+        })
+      ]
     }
   }
 );
