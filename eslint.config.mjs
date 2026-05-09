f54d516792262ddf68322dc5b583e7603537eef0
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2026-05-09 01:24:55 +0100

    Standardize ESLint config import quotes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@
 
 import config from "./index.mjs";
 import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
-import typescriptEslint from 'typescript-eslint';
+import typescriptEslint from "typescript-eslint";
 import { createNodeResolver } from "eslint-plugin-import-x";
 import eslintConfigPrettier from "eslint-config-prettier";
 
